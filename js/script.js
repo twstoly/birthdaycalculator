8ad0cdbce985200776e5b65819197414e9fe70f3
Oliver <oliver.mwangale@moringaschool.com>
2021-11-15 15:21:29 +0300

    update js

--- js/script.js
+++ js/script.js
@@ -1,46 +1,11 @@
 
-
 function calculateBirthday(){
 
     //Get months days from the calendar
     var days = document.getElementById("day").value;
-    alert(days)
+    var day  = parseInt(days)
+    alert(day)
 
-    //Get months value from the calendar
-    var months = document.getElementById("month").value;
-
-    if(months == 1){
-        mewMonthsValue = 11
-    }else if(months == 2){
-        mewMonthsValue = 12;
-    }else if(months == 3){
-        mewMonthsValue = 1
-    }else if(months == 4){
-        newMonthsValue = 2
-    }else if(months == 5){
-        mewMonthsValue = 3
-    }else if(months == 6){
-        newMonthsValue = 4
-    }else if(months == 7){
-        mewMonthsValue = 5
-    }else if(months == 8){
-        newMonthsValue = 6
-    }else if(months == 9){
-        mewMonthsValue = 7
-    }else if(months ==10){
-        newMonthsValue = 8
-    }else if(months == 11){
-        mewMonthsValue = 9
-    }else if(months == 12){
-        newMonthsValue = 10
-    }else{
-        alert("You are out of range")
-    }
-
-    var month = mewMonthsValue;
-    alert(month);
-
-    var year = document.getElementById('year').value;
 
     // Checking for the validity of the items entered..
     if(days <= 0 || days >31){
@@ -56,6 +21,44 @@ function calculateBirthday(){
         alert("You are out of range.");
         document.getElementById("yearFeedback").innerHTML = "..";
     }
+
+    //Get months value from the calendar
+    var months = document.getElementById("month").value;
+
+    if(months == 1){
+        mewMonthsValue = 13
+    }else if(months == 2){
+        mewMonthsValue = 14;
+    }else if(months == 3){
+        mewMonthsValue = 3
+    }else if(months == 4){
+        newMonthsValue = 4
+    }else if(months == 5){
+        mewMonthsValue = 5
+    }else if(months == 6){
+        newMonthsValue = 6
+    }else if(months == 7){
+        mewMonthsValue = 7
+    }else if(months == 8){
+        newMonthsValue = 8
+    }else if(months == 9){
+        mewMonthsValue = 9
+    }else if(months ==10){
+        newMonthsValue = 10
+    }else if(months == 11){
+        mewMonthsValue = 11
+    }else if(months == 12){
+        newMonthsValue = 12
+    }else{
+        alert("You are out of range")
+    }
+
+    var month = parseInt(mewMonthsValue);
+    alert(month);
+
+    var year = document.getElementById('year').value;
+
+
     
     //Get years value from the calendar changing it to an array then slicing the first two values for centuary and the olast two for year.
     output = [];
@@ -67,21 +70,25 @@ function calculateBirthday(){
     // getting the first two values from the years input.
     var firstSecondValues = output[0].toString() + output[1].toString();
     var centuary = parseInt(firstSecondValues)
-
-    alert(firstSecondValues)
+    // alert(centuary)
 
 
     // getting the third and fourth values from the years input.
     var thirdFourthValues = output[2].toString() + output[3].toString()
-    var year = parseInt(thirdFourthValues)
-    alert(thirdFourthValues)
+    var years = parseInt(thirdFourthValues)
+    // alert(thirdFourthValues)
 
-    var dayOfTheWeek = (days + [(13* month - 1)/5] + year + [year/4] + [centuary/4] - 2*centuary);
+    var totalMonths = (month + 1)
 
-    alert(dayOfTheWeek);
+    var specificDay = (day + Math.floor((13 * totalMonths )/5) + years + Math.floor(years/4) + Math.floor(centuary/4) - 2*centuary) % 7;
 
+
+    alert(specificDay);
 }
-document.getElementById("akanName").innerHTML = "Your akan name is: " ;
+
+
+
+// document.getElementById("akanName").innerHTML = "Your akan name is: " ;
 
 
 
